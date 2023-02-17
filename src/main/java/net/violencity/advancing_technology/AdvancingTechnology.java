143ea1d241826079517a6cc67e5745083067f9d5
Violencity <saivarunp.9@gmail.com>
2023-02-17 22:06:41 +0000

    Indev 1.0.0

---
+++ src/main/java/net/violencity/advancing_technology/AdvancingTechnology.java
@@ -0,0 +1,41 @@
+package net.violencity.advancing_technology;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(AdvancingTechnology.MOD_ID)
+public class AdvancingTechnology {
+    public static final String MOD_ID = "advancing_technology";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public AdvancingTechnology() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
